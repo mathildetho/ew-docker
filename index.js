70492c58501854f16bb519f002af14ae78575718
mathildetho <mathilde.thoraninth@gmail.com>
2021-01-05 12:16:44 +0100

    change username api

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ process.on("SIGTERM", () => {
 
     app.get('/', (req, res) => {
         console.log("Got a request");
-        res.json({ message: "Hey, I'm Tom, the API" });
+        res.json({ message: "Hey, I'm Charly, the API" });
     });
 
     app.listen(3000, () => {
